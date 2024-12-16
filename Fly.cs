e4ac0f7a1bf05c34139d0a3c3791eb9ed8eceab5
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-16 20:22:33 +0100

    Target fly

---
+++ Fly.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+using Microsoft.Xna.Framework.Input;
+
+namespace EasyShooter
+{
+    internal class Fly : Actor
+    {
+        public override void Act()
+        {
+            
+            if (IsTouching(typeof(Shot)))
+            {
+                World.RemoveActor(this);
+            }
+        }
+
+    }
+    
+}
