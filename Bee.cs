1cde6a14e08a0a27d0d5bc7f3754a204181ebfb8
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-16 20:22:24 +0100

    Shooting bee.

---
+++ Bee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+using Microsoft.Xna.Framework.Input;
+
+namespace EasyShooter
+{
+    internal class Bee : Actor
+    {
+        private int reloadCountDown = 0;
+        public override void Act()
+        {
+            reloadCountDown -= 1;
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && reloadCountDown < 0)
+            {
+                Shoot();
+                reloadCountDown = 10  ;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                Turn(-5);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Turn(5);
+            }
+
+
+        }
+        private void Shoot()
+        {
+            Shot shot = new Shot();
+            shot.Rotation = this.Rotation;
+
+            World.Add(shot, "red-draught", X, Y);
+        }
+    }
+}
