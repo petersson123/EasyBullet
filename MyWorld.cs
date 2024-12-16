a8a3b3dde9e27b446ed0bd0d3ff7e62dcf52802f
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-16 20:22:53 +0100

    Game world with a bee and many flies.

--- MyWorld.cs
+++ MyWorld.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using EasyMonoGame;
@@ -9,10 +10,32 @@ namespace EasyShooter
 {
     internal class MyWorld : World
     {
-        public MyWorld() : base(600, 800)
+        private Bee bee;
+        private int score = 0;
+        private const int MAX_FLY = 20;
+        public MyWorld() : base(800, 800)
         {
             // Tile background with the file "bluerock" in the Content folder.
             BackgroundTileName = "bluerock";
-        }  
+
+            bee = new Bee();
+            Add(bee, "bee", 400, 400);
+
+            for (int i = 0; i < MAX_FLY; i++)
+            {
+                float x = EasyGame.Instance.Random.Next(0, 800);
+                float y = EasyGame.Instance.Random.Next(0, 800);
+                Add(new Fly(), "fly", x, y);
+            }
+
+        }
+
+        public override void Act()
+        {
+            List<Actor> flies = GetActors(typeof(Fly));
+            int noFlies = flies.Count;
+            score = MAX_FLY - noFlies;
+            ShowText("Score: " + score, 100, 100);
+        }
     }
 }
