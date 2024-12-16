fd188d0c2e263c0376c31cc8dd4b306c4afb8f50
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-16 20:23:07 +0100

    A shot.

---
+++ Shot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+
+namespace EasyShooter
+{
+    internal class Shot : Actor
+    {
+        public override void Act()
+        {
+            Move(20);
+            if ( IsAtEdge())
+            {
+                World.RemoveActor(this);
+            }
+        }
+
+    }
+}
