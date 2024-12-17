167b8c221c352d59e92ab4a589d71db5f13ff01f
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-17 09:04:18 +0100

    Shoot cool down.

--- Bee.cs
+++ Bee.cs
@@ -10,18 +10,28 @@ namespace EasyBullet
 {
     internal class Bee : Actor
     {
-
+        private int timeToShoot = 0;
         public override void Act()
         {
+            timeToShoot -= 1;
 
             this.TurnTowards(Mouse.GetState().X, Mouse.GetState().Y);
             if (Keyboard.GetState().IsKeyDown(Keys.Space))
             {
-                Bullet bullet = new Bullet();
-                World.Add(bullet, "red-draught", this.X, this.Y);
-                bullet.Rotation = this.Rotation;
-                bullet.Move(50);
+                if (timeToShoot <= 0)
+                {
+                    Shoot();
+                    timeToShoot = 20;
+                }
+                
             }
+        }
+        private void Shoot()
+        {
+            Bullet bullet = new Bullet();
+            World.Add(bullet, "red-draught", this.X, this.Y);
+            bullet.Rotation = this.Rotation;
+            bullet.Move(50);
         }
     }
 }
